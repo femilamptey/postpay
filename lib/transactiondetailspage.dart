b8006decc895eb54347a6249e2d684bb6068e1df
Femi Lamptey <femi.lamptey@gmail.com>
2019-11-23 02:52:55 +1100

    Update transactiondetailspage.dart

--- lib/transactiondetailspage.dart
+++ lib/transactiondetailspage.dart
@@ -2,11 +2,18 @@ import 'package:afterpay/mtnmobilemoney.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TransactionDetailsPage extends StatelessWidget {
+class TransactionDetailsPage extends StatefulWidget {
 
   TransactionDetailsPage(this._transaction);
 
   final AfterPayTransaction _transaction;
+
+  @override
+  _TransactionDetailsPageState createState() => _TransactionDetailsPageState();
+
+}
+
+class _TransactionDetailsPageState extends State<TransactionDetailsPage> {
 
   Text transactionDetails(AfterPayTransaction transaction) {
     String details = "Payment plan: ${transaction.planAsString()}\n"
@@ -32,29 +39,29 @@ class TransactionDetailsPage extends StatelessWidget {
     );
 
     final initiateNextPaymentButton = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: RaisedButton(
-        child: Text('Complete Next Transaction'),
-        color: Colors.black,
-        textColor: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
-        onPressed: () {
-          // Perform some action
-        },
-      )
+        padding: EdgeInsets.all(8.0),
+        child: RaisedButton(
+          child: Text('Complete Next Transaction'),
+          color: Colors.black,
+          textColor: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.0)),
+          onPressed: () {
+            // Perform some action
+          },
+        )
     );
 
     var body = ListView(
       children: <Widget>[
         Card(
           child: ListTile(
-            title: Text("Transaction ${_transaction.financialTransactionID}", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w500), textAlign: TextAlign.center),
+            title: Text("Transaction ${widget._transaction.financialTransactionID}", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w500), textAlign: TextAlign.center),
           ),
         ),
         Card(
           child: ListTile(
-            title: Text("Payee: ${_transaction.payee}", style: TextStyle(color: Colors.black, fontSize: 21.0), textAlign: TextAlign.center),
-            subtitle: transactionDetails(_transaction),
+            title: Text("Payee: ${widget._transaction.payee}", style: TextStyle(color: Colors.black, fontSize: 21.0), textAlign: TextAlign.center),
+            subtitle: transactionDetails(widget._transaction),
           ),
         ),
         initiateNextPaymentButton
